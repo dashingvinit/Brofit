c1ae7176e2eb9a307c421435d2888e85a61b8c5d
Uday Pratap Singh <udaypratapsingh95802@gmail.com>
2023-07-21 18:02:12 +0530

    h

--- src/owner/ActiveList.js
+++ src/owner/ActiveList.js
@@ -37,7 +37,7 @@ const ActiveList = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: bgColor }}>
       <View style={{ marginTop: 20, alignItems: 'center' }}>
-        <Text style={styles.heading}>Inactive Members</Text>
+        <Text style={styles.heading}>Active Members</Text>
       </View>
       <ScrollView>
         {loading ? (
