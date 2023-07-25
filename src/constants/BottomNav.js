3fc30dbc7a478b6f533d2f0fd9a4bca530bc8cae
dashingvinit <21bcs7277@cuchd.in>
2023-07-25 13:59:28 +0530

    ownerUI

--- src/constants/BottomNav.js
+++ src/constants/BottomNav.js
@@ -42,15 +42,6 @@ const BottomNav = ({ setHandleLogout }) => {
       activeColor={neon}
       inactiveColor="#3e2465">
       <Tab.Screen
-        name="HomeStack"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="ios-home-outline" color={color} size={30} />
-          ),
-        }}>
-        {(props) => <HomeStack {...props} setHandleLogout={setHandleLogout} />}
-      </Tab.Screen>
-      <Tab.Screen
         name="WorkoutStack"
         component={WorkoutStack}
         options={{
@@ -68,6 +59,16 @@ const BottomNav = ({ setHandleLogout }) => {
           ),
         }}
       />
+
+      <Tab.Screen
+        name="HomeStack"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" color={color} size={30} />
+          ),
+        }}>
+        {(props) => <HomeStack {...props} setHandleLogout={setHandleLogout} />}
+      </Tab.Screen>
 
       <Tab.Screen
         name="Timer"
