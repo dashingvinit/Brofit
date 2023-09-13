676b59fe1acc60a095fe9beb07f11a3efce906dd
Uday Pratap Singh <udaypratapsingh95802@gmail.com>
2023-09-13 19:27:14 +0530

    notification done completely

--- app/components/Announcement.js
+++ app/components/Announcement.js
@@ -1,13 +1,39 @@
 import React,{ useState } from 'react'
 import { View, Text,TextInput,TouchableOpacity,StyleSheet } from 'react-native'
-import { neon } from '../constants/Constants'
+import { bgColor, neon } from '../constants/Constants'
+import axios from '../constants/Axios';
+import * as SecureStore from 'expo-secure-store';
+import LottieView from 'lottie-react-native';
 
 const Announcement = () => {
 
-    const [message,setMessage] = useState('')
+  const [message,setMessage] = useState('')
+  const [loading, setLoading] = useState(false);
+
+  const msgsend = async() =>{
+    setLoading(true);
+    try {
+        const userString = await SecureStore.getItemAsync('user');
+        const user = JSON.parse(userString);
+        const Id = user.gymId;
+        const response = await axios.post(`/noti/${Id}`, {
+            content: message,
+          });
+        //   console.log('Response:', response.data.data);
+        setMessage('')
+        setLoading(false);
+      } 
+      catch (error) {
+        if (error.response && error.response.status === 404) {
+          console.log(error);
+        } else {
+          console.error('Error in pushing notification data:', error);
+        }
+    }
+  }
 
   return (
-    <View style={{marginTop:50}}>
+    <View style={{marginTop:50,backgroundColor:bgColor,paddingBottom:50,paddingTop:50,borderRadius:25,margin:15}}>
         <View style={{alignItems:'center',justifyContent:'center'}}>
             <Text style={{color:neon,fontSize:24}}> Announcements </Text>
         </View>
@@ -18,10 +44,29 @@ const Announcement = () => {
             value={message}
             onChangeText={setMessage}
         />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={msgsend}>
             <Text style={{fontSize:20}}>Send</Text>
         </TouchableOpacity>
         </View>
+        {loading && (
+        <View
+          style={{
+            position: 'absolute',
+            top: -400,
+            bottom: -50,
+            left: 0,
+            right: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <LottieView
+            source={require('../assets/lottieFiles/greenTik.json')}
+            autoPlay
+            loop
+          />
+        </View>
+      )}
     </View>
   )
 }
@@ -37,8 +82,8 @@ input: {
 button : {
     backgroundColor:neon,
     height:40,
-    width:80,
-    borderRadius:25,
+    width:100,
+    borderRadius:10,
     alignItems:'center',
     justifyContent:'center'
 },
--- app/components/Notification.js
+++ app/components/Notification.js
@@ -1,91 +1,144 @@
-import React,{useState,useEffect} from 'react'
-import { SafeAreaView,
-         View,
-         Text,
-         StyleSheet,
-         ActivityIndicator
-} from 'react-native'
-import {TopBack,GradientBG } from '../components';
-import { bgGlass, neon, bgColor, bgLight } from '../constants/Constants';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView
+} from 'react-native';
+import TopBack from '../components/TopBack';
+import GradientBG from '../components/GradientBG';
+import { neon, bgLight, bgColor } from '../constants/Constants';
 import axios from '../constants/Axios';
 import * as SecureStore from 'expo-secure-store';
 
 const Notification = () => {
-    const [inactiveData, setInactiveData] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [inactiveData, setInactiveData] = useState([]);
+  const [personalData, setPersonalData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const fetchData = async () => {
-        try {
-          const userString = await SecureStore.getItemAsync('user');
-          const user = JSON.parse(userString);
-          console.log(user?.userId);
-          
-          setLoading(false);
-        } catch (error) {
-          if (error.response && error.response.status === 404) {
-            console.log('Data not found');
-          } else {
-            console.error('Error fetching notification data:', error);
-          }
-          setLoading(false);
-        }
-      };
+  const fetchData = async () => {
+    try {
+      const userString = await SecureStore.getItemAsync('user');
+      const user = JSON.parse(userString);
+    //   console.log(user.gymId)
+    //   console.log(user.userId)
+      const responseInactive = await axios.get(`/noti/${user.gymId}`);
+      const responsePersonal = await axios.get(`/noti/spec/${user.gymId}/${user.userId}`);
 
-    useEffect(() => {
-        fetchData();
-      }, []);
+      setInactiveData(responseInactive.data.data);
+      setPersonalData(responsePersonal.data.data);
+      // console.log(responseInactive.data);
+      // console.log(responsePersonal.data);
+
+      setLoading(false);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        console.log('Data not found');
+      } else {
+        console.error('Error fetching notification data:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderNotifications = (data) => {
+    return data.map((message, index) => (
+      <View key={index} style={styles.dataContainer} style={styles.containerdata}>
+        <Text style={styles.dataItem1}>{message.content}</Text>
+        <View style={styles.dataContainer1}>
+        <Text style={styles.dataItem2}>{message.day}</Text>
+        <View>
+          <Text style={styles.dataItem2}>{message.time}</Text>
+        </View>
+        {/* <Text style={styles.dataItem2}>{message.gymId}</Text> */}
+        </View>
+        <View style={styles.separator} />
+      </View>
+    ));
+  };
 
   return (
     <GradientBG>
-        <View style={{flex:1,marginTop:40}}>
+      <View style={{ flex: 1, marginTop: 40 }}>
         <TopBack>Notification</TopBack>
+        <ScrollView>
         <View style={styles.scrollContainer}>
-            {loading ? (
-              <ActivityIndicator size="large" />
-            ) : inactiveData.length > 0 ? (
-              inactiveData.map((member, index) => (
-                <TouchableOpacity
-                  key={index}
-                  style={styles.dataContainer}
-                  onPress={() => handleUserPress(member)}>
-                  <View style={styles.flexRow}>
-                    <Text style={styles.dataItem1}>{member?.userId?.name}</Text>
-                    <Text style={styles.dataItem1}>
-                      {member?.userId?.registerationNumber}
-                    </Text>
-                  </View>
-                  <View style={{flexDirection:'row'}}>
-                    <TouchableOpacity
-                      onPress={() => handleEdit(member?.userId?._id)}>
-                      <Text style={styles.editButton}>Activate</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                      onPress={() => handlePing(member?.userId?._id)}>
-                      <Text style={styles.editButton1}>Ping</Text>
-                    </TouchableOpacity>
-                  </View>
-                </TouchableOpacity>
-              ))
-            ) : (
-              <View style={{alignItems:'center',marginTop:20}}>
-                <Text style={{color:neon,fontSize:20}}> No Notifications </Text>
+          {loading ? (
+            <ActivityIndicator size="large" />
+          ) : (
+            <>
+              <View style={{backgroundColor:bgColor,borderRadius:15,margin:15,paddingBottom:15}}>
+                <Text style={styles.text1}>Announcements</Text>
+                {inactiveData.length > 0 ? renderNotifications(inactiveData) : (
+                    <View style={{ alignItems: 'center', marginTop: 20 }}>
+                        <Text style={{ color: neon, fontSize: 20 }}>No Inactive Messages</Text>
+                    </View>
+                )}
               </View>
-            )}
-            </View>
+              <View style={{backgroundColor:bgColor,borderRadius:15,margin:15,marginBottom:85}}>
+              <Text style={{ color: neon, fontSize: 20, fontWeight:'bold' ,marginTop: 20, marginBottom: 10,marginLeft:20,marginTop:15,textAlign:'center' }}>Personal Notification</Text>
+              {personalData.length > 0 ? renderNotifications(personalData) : (
+                <View style={{ alignItems: 'center', marginTop: 20 }}>
+                  <Text style={{ color: neon, fontSize: 20 }}>No Personal Messages</Text>
+                </View>
+              )}
+              </View>
+            </>
+          )}
         </View>
+      </ScrollView>
+      </View>
     </GradientBG>
-  )
-}
+  );
+};
+
 const styles = StyleSheet.create({
-    notify: {
-        backgroundColor : bgLight,
-        height:50,
-        width:300,
-        fontSize:20,
-        color:neon,
-        
-    }
+  dataContainer: {
+    padding: 10,
+    borderRadius: 15,
+    margin: 15,
+  },
+  text1:{
+    color: neon, 
+    fontSize: 20,
+    fontWeight:'bold', 
+    marginBottom: 10,
+    marginLeft:20,
+    marginTop:15,
+    textAlign:'center'
+  },
+  dataContainer1: {
+    paddingTop:10,
+    borderRadius: 15,
+    margintop: 15, 
+    flexDirection:'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginTop:5
+  },
+  dataItem1: {
+    color: "white",
+    fontSize: 18,
+    marginBottom:10,
+  },
+  dataItem2: {
+    color: "white",
+    fontSize: 12,
+    
+  },
+  containerdata: {
+    backgroundColor:bgColor,
+    marginBottom:10,
+    borderRadius:20,
+    padding:15
+  },
 });
 
-
-export default Notification
+export default Notification;
